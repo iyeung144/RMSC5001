ab64730d61e2b1cb33e8d34474cd65c8f4bc3525
iyeung144 <ivan.kmyeung@outlook.com>
2019-05-09 23:09:19 +0800

    update csv and plain R

---
+++ RMSC5001_Project_2018-2019-Classification_Tree.R
@@ -0,0 +1,200 @@
+## ----setup, include=FALSE------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE, dpi = 300, fig.width = 7)
+
+
+## ----set-options, echo=FALSE, cache=FALSE--------------------------------
+options(width = 150)
+
+
+## ----load packages, message=FALSE, warning=FALSE-------------------------
+library(ggplot2)
+library(ggthemes)
+library(gridExtra)
+library(rworldmap)
+library(knitr)
+library(rpart)
+library(rpart.plot)
+library(tidyverse)
+
+
+## ----data field, echo=FALSE, results='asis'------------------------------
+income <- read.csv("worldbank_income.csv")
+kable(income, caption = "World Bank definition on the income group")
+
+field <- read.csv("worldbank_field.csv")
+kable(field, caption = "Field information")
+
+
+## ----read data-----------------------------------------------------------
+d <- read.csv("world.csv", header = TRUE)
+n <- nrow(d) # get sample size
+set.seed(12345) # set random seed
+r <- 0.80 # set sampling ratio
+id <- sample(1:n, size = round(r * n)) # generate id
+train <- d[id, ] # training dataset
+test <- d[-id, ]
+head(d)
+
+
+## ----world map-----------------------------------------------------------
+income.df <- joinCountryData2Map(d,
+  joinCode = "NAME",
+  nameJoinColumn = "COUNTRY"
+)
+
+mapParams <- mapCountryData(income.df,
+  nameColumnToPlot = "INCOME_GROUP",
+  catMethod = "categorical",
+  colourPalette = "diverging",
+  addLegend = "FALSE",
+  mapTitle = "Different Income Levels around the World"
+)
+
+do.call(
+  addMapLegendBoxes,
+  c(mapParams,
+    x = "bottomleft",
+    horiz = FALSE,
+    title = "",
+    bg = "NA",
+    col = "NA"
+  )
+)
+
+
+## ----plot income group number--------------------------------------------
+# All data
+prop.table(table(d$INCOME_GROUP))
+all_plot <- d[, -1] %>%
+  count(INCOME_GROUP) %>%
+  ggplot() +
+  geom_col(aes(x = INCOME_GROUP, y = n),
+    fill = "#002244",
+    colour = "#002244"
+  ) +
+  ggtitle("Income group (All data)") +
+  coord_flip() +
+  theme_fivethirtyeight()
+
+# Training data
+prop.table(table(train$INCOME_GROUP))
+train_plot <- train[, -1] %>%
+  count(INCOME_GROUP) %>%
+  ggplot() +
+  geom_col(aes(x = INCOME_GROUP, y = n),
+    fill = "#002244",
+    colour = "#002244"
+  ) +
+  ggtitle("Income group (Training data)") +
+  coord_flip() +
+  theme_fivethirtyeight()
+
+# Testing data
+prop.table(table(test$INCOME_GROUP))
+test_plot <- test[, -1] %>%
+  count(INCOME_GROUP) %>%
+  ggplot() +
+  geom_col(aes(x = INCOME_GROUP, y = n),
+    fill = "#002244",
+    colour = "#002244"
+  ) +
+  ggtitle("Income group (Testing data)") +
+  coord_flip() +
+  theme_fivethirtyeight()
+
+grid.arrange(all_plot, train_plot, test_plot, nrow = 3, ncol = 1)
+
+
+## ----grow classification tree--------------------------------------------
+ctree <- rpart(INCOME_GROUP ~
+REGION + GDP +
+  LABOUR_FORCE +
+  LIFE_EXPECTANCY +
+  MILITARY_EXPENSE +
+  HIGH_TECHNOLOGY_EXPORT +
+  CORRUPTION,
+data = train,
+method = "class",
+control = rpart.control(cp = 0)
+)
+
+
+## ----plot classification tree with probability---------------------------
+# plot the tree with option 104 for probability
+rpart.plot(ctree, extra = 104)
+
+
+## ----plot classification tree with numbers-------------------------------
+# plot the tree with option 101 for numbers
+rpart.plot(ctree, extra = 101)
+
+
+## ----print the classification tree---------------------------------------
+print(ctree)
+
+
+## ----print rules---------------------------------------------------------
+rpart.rules(ctree)
+
+
+## ----rule results, echo=FALSE, results='asis'----------------------------
+rule.results <- read.csv("rule_results.csv")
+kable(rule.results, caption = "Classification Tree Explanatory Power")
+
+
+## ----tree complexity-----------------------------------------------------
+printcp(ctree)
+plotcp(ctree)
+
+
+## ----training prediction-------------------------------------------------
+predict_train2 <- predict(ctree, train, type = "class")
+table(predict_train2, train$INCOME_GROUP)
+
+
+## ----testing prediction--------------------------------------------------
+predict_test2 <- predict(ctree, test, type = "class")
+table(predict_test2, test$INCOME_GROUP)
+
+
+## ----base classification accuracy----------------------------------------
+base_accuracy <- mean(predict_test2 == test$INCOME_GROUP)
+(base_accuracy)
+
+
+## ----train prediction, echo=FALSE, results='asis'------------------------
+prediction.training <- read.csv("prediction_training.csv")
+kable(prediction.training, caption = "Results on the prediction of training data")
+
+
+## ----test prediction, echo=FALSE, results='asis'-------------------------
+prediction.testing <- read.csv("prediction_testing.csv")
+kable(prediction.testing, caption = "Results on the prediction of training data")
+
+
+## ----pre-pruning---------------------------------------------------------
+# Grow a tree with minsplit of 100 and max depth of 8
+ctree_preprun <- rpart(INCOME_GROUP ~
+REGION + GDP +
+  LABOUR_FORCE +
+  LIFE_EXPECTANCY +
+  MILITARY_EXPENSE +
+  HIGH_TECHNOLOGY_EXPORT +
+  CORRUPTION,
+data = train, method = "class",
+control = rpart.control(cp = 0, maxdepth = 3, minsplit = 20)
+)
+# Compute the accuracy of the pruned tree
+test$pred <- predict(ctree_preprun, test, type = "class")
+accuracy_preprun <- mean(test$pred == test$INCOME_GROUP)
+
+
+## ----post-pruning--------------------------------------------------------
+# Prune the ctree_model based on the optimal cp value
+ctree_pruned <- prune(ctree, cp = 0.046)
+
+# Compute the accuracy of the pruned tree
+test$pred <- predict(ctree_pruned, test, type = "class")
+accuracy_postprun <- mean(test$pred == test$INCOME_GROUP)
+data.frame(base_accuracy, accuracy_preprun, accuracy_postprun)
+
---
+++ RMSC5001_Project_2018-2019-PCA.R
@@ -0,0 +1,320 @@
+## ----setup, include=FALSE------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE, dpi = 300, fig.width = 7)
+
+
+## ----load packages, message=FALSE, warning=FALSE-------------------------
+library(corrplot)
+library(ggplot2)
+library(ggthemes)
+library(tidyverse)
+library(zoo)
+library(factoextra)
+library(FactoMineR)
+library(knitr)
+
+
+## ----echo=FALSE, results='asis'------------------------------------------
+field <- read.csv("field.csv")
+kable(field, caption = "Field information")
+
+
+## ----read data-----------------------------------------------------------
+set.seed(123456)
+d <- read.csv("season-1718_csv.csv", stringsAsFactors = FALSE)
+d1 <- d %>%
+  group_by(HomeTeam) %>%
+  summarise(
+    HS = sum(HS),
+    AS = sum(AS),
+    HST = sum(HST),
+    AST = sum(AST),
+    HF = sum(HF),
+    AF = sum(AF),
+    HC = sum(HC),
+    AC = sum(AC),
+    HY = sum(HY),
+    AY = sum(AY),
+    HR = sum(HR),
+    AR = sum(AR)
+  )
+league.df <- column_to_rownames(d1, var = "HomeTeam")
+(league.df)
+summary(league.df)
+ggplot(stack(league.df), aes(x = ind, y = values)) +
+  geom_boxplot(fill = "#3d195b", colour = "red") +
+  labs(
+    x = "Variables",
+    y = "Values",
+    title = "Boxplot for 12 variables"
+  )
+
+
+## ----correlation plot----------------------------------------------------
+r <- cor(league.df)
+corrplot.mixed(r, lower = "square", upper = "number", order = "FPC")
+
+
+## ----pca-----------------------------------------------------------------
+pca <- prcomp(league.df, center = TRUE, scale = TRUE)
+summary(pca)
+
+
+## ----scree plot----------------------------------------------------------
+fviz_screeplot(pca,
+  addlabels = TRUE, ylim = c(0, 50),
+  main = "Screeplot of the first 10 PCs",
+  barfill = "#3d195b", barcolor = "#3d195b"
+)
+
+
+## ----pca variance--------------------------------------------------------
+var <- get_pca_var(pca)
+corrplot(var$cos2, is.corr = FALSE, method = "square")
+# Total cos2 of variables on PC1 and PC2
+fviz_cos2(pca,
+  choice = "var", axes = 1:2,
+  title = "Sum of independent variables in PC1 and PC2",
+  fill = "#3d195b", color = "#3d195b"
+)
+
+
+## ----eigenvalues analysis------------------------------------------------
+zapsmall(get_eigenvalue(pca))
+
+
+## ----loadings of pc------------------------------------------------------
+(zapsmall(var$contrib[, 1:5]))
+
+
+## ----plot loadings contribution to pc1-----------------------------------
+# Contributions of variables to PC1
+fviz_contrib(pca, choice = "var", axes = 1, top = 12, fill = "#3d195b", color = "#3d195b")
+
+
+## ----pc2-----------------------------------------------------------------
+# Contributions of variables to PC2
+fviz_contrib(pca, choice = "var", axes = 2, top = 12, fill = "#3d195b", color = "#3d195b")
+
+
+## ----pc3-----------------------------------------------------------------
+# Contributions of variables to PC3
+fviz_contrib(pca, choice = "var", axes = 3, top = 12, fill = "#3d195b", color = "#3d195b")
+
+
+## ----pc4-----------------------------------------------------------------
+# Contributions of variables to PC4
+fviz_contrib(pca, choice = "var", axes = 4, top = 12, fill = "#3d195b", color = "#3d195b")
+
+
+## ----pc5-----------------------------------------------------------------
+# Contributions of variables to PC5
+fviz_contrib(pca, choice = "var", axes = 5, top = 12, fill = "#3d195b", color = "#3d195b")
+
+
+## ----scatter plot--------------------------------------------------------
+score1 <- pca$x[, 1:5]
+pairs(score1)
+
+
+## ----pca var-------------------------------------------------------------
+fviz_pca_var(pca,
+  col.var = "cos2",
+  gradient.cols = c("black", "blue", "red"),
+  repel = TRUE # Avoid text overlapping
+)
+
+
+## ----clubs contribution of each pc---------------------------------------
+fviz_contrib(pca,
+  choice = "ind",
+  axes = 1,
+  fill = "#3d195b",
+  color = "#3d195b",
+  title = "Total Contribution of clubs to PC1"
+)
+fviz_contrib(pca,
+  choice = "ind",
+  axes = 2,
+  fill = "#3d195b",
+  color = "#3d195b",
+  title = "Total Contribution of clubs to PC2"
+)
+fviz_contrib(pca,
+  choice = "ind",
+  axes = 3,
+  fill = "#3d195b",
+  color = "#3d195b",
+  title = "Total Contribution of clubs to PC3"
+)
+fviz_contrib(pca,
+  choice = "ind",
+  axes = 4,
+  fill = "#3d195b",
+  color = "#3d195b",
+  title = "Total Contribution of clubs to PC4"
+)
+fviz_contrib(pca,
+  choice = "ind",
+  axes = 5,
+  fill = "#3d195b",
+  color = "#3d195b",
+  title = "Total Contribution of clubs to PC5"
+)
+
+
+## ----biplot--------------------------------------------------------------
+fviz_pca_biplot(pca,
+  repel = TRUE,
+  col.var = "blue", # Variables color
+  col.ind = "#3d195b" # Individuals color
+)
+
+
+## ----loadings------------------------------------------------------------
+(Loadings <- pca$rotation[, 1:5] %>%
+  round(2) %>%
+  data.frame() %>%
+  mutate(Attribute = rownames(.)) %>%
+  select(Attribute, everything()) %>%
+  arrange(PC1))
+
+
+## ----time series of clubs------------------------------------------------
+d$PC1 <- pca$x[, 1]
+d$PC2 <- pca$x[, 2]
+
+d %>%
+  ggplot() +
+  geom_line(aes(x = as.Date(Date), y = PC1, group = 1), show.legend = F, colour = "#3d195b") +
+  geom_hline(yintercept = 0, colour = "red") +
+  facet_wrap(~HomeTeam) +
+  labs(
+    x = "Date",
+    y = "PC1",
+    title = "PC1 time series by clubs"
+  ) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  scale_x_date(date_labels = "%Y (%b)")
+
+
+## ----comparison----------------------------------------------------------
+d %>%
+  ggplot() +
+  geom_line(aes(x = as.Date(Date), y = PC2, group = 1), show.legend = F, colour = "#3d195b") +
+  geom_hline(yintercept = 0, colour = "red") +
+  facet_wrap(~HomeTeam) +
+  labs(
+    x = "Date",
+    y = "PC2",
+    title = "PC2 time series by clubs"
+  ) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  scale_x_date(date_labels = "%Y (%b)")
+
+
+## ----pc1 and pc2 mean----------------------------------------------------
+d %>%
+  group_by(HomeTeam) %>%
+  summarise(
+    Index.1 = mean(PC1),
+    Index.2 = mean(PC2)
+  ) %>%
+  arrange(desc(Index.1)) %>%
+  select(HomeTeam, Index.1, Index.2)
+
+
+## ----rolling mean of pc1 and pc2-----------------------------------------
+d %<>% group_by(HomeTeam) %>%
+  arrange(Date) %>%
+  mutate(
+    M.A.PC1 = rollmeanr(PC1, 15, fill = NA),
+    M.A.PC2 = rollmeanr(PC2, 15, fill = NA)
+  )
+
+Index.1718 <- d %>%
+  group_by(HomeTeam) %>%
+  filter(Date == max(Date)) %>%
+  select(HomeTeam, M.A.PC1, M.A.PC2, Date) %>%
+  arrange(M.A.PC1)
+
+ggplot(Index.1718, aes(x = M.A.PC1, y = M.A.PC2)) +
+  geom_point(show.legend = F, colour = "#3d195b", size = 5) +
+  geom_text(aes(label = HomeTeam),
+    check_overlap = TRUE, nudge_y = 0.08,
+    show.legend = F, size = 2.9
+  ) +
+  labs(x = "PC1", y = "PC2")
+
+
+## ----away team analysis--------------------------------------------------
+d2 <- d %>%
+  group_by(AwayTeam) %>%
+  summarise(
+    HS = sum(HS),
+    AS = sum(AS),
+    HST = sum(HST),
+    AST = sum(AST),
+    HF = sum(HF),
+    AF = sum(AF),
+    HC = sum(HC),
+    AC = sum(AC),
+    HY = sum(HY),
+    AY = sum(AY),
+    HR = sum(HR),
+    AR = sum(AR)
+  )
+away.df <- column_to_rownames(d2, var = "AwayTeam")
+(away.df)
+summary(away.df)
+ggplot(stack(away.df), aes(x = ind, y = values)) +
+  geom_boxplot(fill = "#3d195b", colour = "red") +
+  labs(
+    x = "Variables",
+    y = "Values",
+    title = "Boxplot for 12 variables"
+  )
+pca2 <- prcomp(away.df, center = TRUE, scale = TRUE)
+summary(pca2)
+fviz_pca_var(pca2,
+  col.var = "cos2",
+  gradient.cols = c("black", "blue", "red"),
+  repel = TRUE # Avoid text overlapping
+)
+d$PC1a <- pca2$x[, 1]
+d$PC2a <- pca2$x[, 2]
+
+d %>%
+  ggplot() +
+  geom_line(aes(x = as.Date(Date), y = PC1a, group = 1), show.legend = F, colour = "#3d195b") +
+  geom_hline(yintercept = 0, colour = "red") +
+  facet_wrap(~HomeTeam) +
+  labs(
+    x = "Date",
+    y = "PC1a",
+    title = "PC1a time series by clubs (Away)"
+  ) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  scale_x_date(date_labels = "%Y (%b)")
+
+d %>%
+  ggplot() +
+  geom_line(aes(x = as.Date(Date), y = PC2a, group = 1), show.legend = F, colour = "#3d195b") +
+  geom_hline(yintercept = 0, colour = "red") +
+  facet_wrap(~AwayTeam) +
+  labs(
+    x = "Date",
+    y = "PC2a",
+    title = "PC2a time series by clubs (Away)"
+  ) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  scale_x_date(date_labels = "%Y (%b)")
+
+d %>%
+  group_by(AwayTeam) %>%
+  summarise(
+    Index.1 = mean(PC1a),
+    Index.2 = mean(PC2a)
+  ) %>%
+  arrange(desc(Index.1)) %>%
+  select(AwayTeam, Index.1, Index.2)
+
